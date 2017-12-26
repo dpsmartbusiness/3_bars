50af4d0d6f68c81f43a27dca6917e37f2da24987
dpsmartbusiness <33832856+dpsmartbusiness@users.noreply.github.com>
2017-12-26 15:02:40 +0500

    pep-8 and snake_case formatted

--- bars.py
+++ bars.py
@@ -11,21 +11,24 @@ def load_data(file_path):
 def get_biggest_bar(bars):
     b_bar = dict()
     for bar in bars["features"]:
-        b_bar[bar["properties"]["Attributes"]["SeatsCount"]] = bar["properties"]["Attributes"]["Name"]
+        b_bar[bar["properties"]["Attributes"]["SeatsCount"]] = \
+            bar["properties"]["Attributes"]["Name"]
     return b_bar.get(max(b_bar))
 
 
 def get_smallest_bar(bars):
     s_bar = dict()
     for bar in bars["features"]:
-        s_bar[bar["properties"]["Attributes"]["SeatsCount"]] = bar["properties"]["Attributes"]["Name"]
+        s_bar[bar["properties"]["Attributes"]["SeatsCount"]] = \
+            bar["properties"]["Attributes"]["Name"]
     return s_bar.get(min(s_bar))
 
 
 def get_closest_bar(bars, longitude, latitude):
     bar_coord = dict()
     for bar in bars["features"]:
-        bar_coord[bar["properties"]["Attributes"]["Name"]] = bar['geometry']['coordinates']
+        bar_coord[bar["properties"]["Attributes"]["Name"]] = \
+            bar['geometry']['coordinates']
     c_bar = dict()
     for bar_key in bar_coord.keys():
         c_bar[get_min(bar_coord[bar_key], [longitude, latitude])] = bar_key
@@ -39,15 +42,24 @@ def get_min(l1, l2):
 
 
 if __name__ == '__main__':
-    print("Критерий для поиска бара: \n 1. Самый большой бар \n 2. Самый маленький бар \n 3. Ближайший бар")
-    answ = int(input("Задайте требуемый критерий для поиска цифрой от 1го до 3х: \n"))
+    print("Критерий для поиска бара: \n "
+          "1. Самый большой бар \n "
+          "2. Самый маленький бар \n "
+          "3. Ближайший бар")
+    answ = int(
+        input("Задайте требуемый критерий для поиска цифрой от 1го до 3х: \n"))
 
     if (answ == 1):
-        print("Cамый большой бар: ", get_biggest_bar(load_data(sys.argv[1])))
+        print("Cамый большой бар: ",
+              get_biggest_bar(load_data(sys.argv[1])))
     elif (answ == 2):
-        print("Самый маленький бар: ", get_smallest_bar(load_data(sys.argv[1])))
+        print("Самый маленький бар: ",
+              get_smallest_bar(load_data(sys.argv[1])))
     elif (answ == 3):
-        userCoord = [float(u_coord) for u_coord in input('Ввведите координаты через запятую: ').split(',')]
-        print("Ближайший бар: ", get_closest_bar(load_data(sys.argv[1]), userCoord[0], userCoord[1]))
+        user_Coord = [float(u_coord) for u_coord in
+                     input('Ввведите координаты через запятую: ').split(',')]
+        print("Ближайший бар: ",
+              get_closest_bar(load_data(sys.argv[1]), user_Coord[0],
+                              user_Coord[1]))
     else:
         print("Некорректный формат ввода данных")
